59fab5aaefdd4d44dcae904c29db0e9c5637074f
bongiBong-create <mairanss543@inbox.ru>
2024-10-04 13:37:26 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,19 +5,20 @@ const temp = document.querySelector(".city__info-temp");
 const search = document.querySelector(".search");
 const btn = document.querySelector('.add__city');
 const city = document.querySelector('.city');
+const time = document.querySelector('.time');
 
 let location = "Saint-Petersburg";
 
 btn.addEventListener("click", getWeather)
 
-function getWeather () {
-    location = search.value
+function getWeather() {
+    location = search.value;
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${weather}`)
         .then(response => {
             return response.json()
         })
         .then(response => {
-            const responseTemp = Math.round((response.days[0].temp - 32) * 5/9);
+            const responseTemp = Math.round((response.days[0].temp - 32) * 5 / 9) + "°";
             temp.textContent = responseTemp;
             city.textContent = response.address;
             console.log(response)
@@ -26,16 +27,8 @@ function getWeather () {
             console.error(err);
         });
 }
-fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${weather}`)
-.then(response => {
-    return response.json()
-})
-.then(response => {
-    // temp.textContent = response.
 
-    console.log(response)
-})
-.catch(err => {
-    console.error(err);
-});
-
+setInterval(function () {
+    const date = new Date();
+    time.innerText = date.getHours() + ":" + date.getMinutes() + "-"
+}, 1000)
